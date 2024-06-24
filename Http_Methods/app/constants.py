1bd87b158a89c5f4e32bf334f35a5f961ca4cbc6
komal050505 <komal050505@gmail.com>
2024-06-24 23:24:28 +0530

    Description of the changes made today

--- Http_Methods/app/constants.py
+++ Http_Methods/app/constants.py
@@ -1,5 +1,25 @@
-users = {"923456789": {"name": "Suresh", "email": "Suresh@example.com"},
-         "9987654321": {"name": "Bobby", "email": "bobby@example.com"},
-         "9541322125": {"name": "Andrew", "email": "andrew@example.com"},
-         "9987654264": {"name": "Rajesh", "email": "rajesh@example.com"},
-         "9554542122": {"name": "Mahesh", "email": "mahesh@example.com"}}
+"""
+
+This module is for constants which contains Data Base related Data
+
+"""
+# DATABASE RECORD
+data = {"records": [{"mobile": 914234234245, "name": "Kumar Makala", "company": "KXN"},
+                    {"mobile": 915421215452, "name": "Komal", "company": "APPLE"},
+                    {"mobile": 913020022100, "name": "Mahesh", "company": "MICROSOFT"},
+                    {"mobile": 910000000000, "name": "Rajesh", "company": "WIPRO"},
+                    {"mobile": 914111111111, "name": "Suresh", "company": "MIND TREE"}]}
+"""
+914111111111 in data
+"records" in data
+"""
+
+available_records = [item["mobile"] for item in data["records"]]
+# print(f"available records are --- {available_records}")
+
+
+# FILTERS TO BE APPLIED
+VALID_COUNTRY_LIST = ["91", "45", "67", "56"]
+EXCLUDED_NUMBERS = [9898989898, 9999999999, 8888888888]
+
+
